da24bba0bb3f92e7cb329016f8d9c397ba71b04a
Mociej <maciekkocemba@gmail.com>
2021-10-14 12:55:32 +0200

    basic controller function

--- PPKWUzad1/src/main/java/pkkwuzad1/ReverseController.java
+++ PPKWUzad1/src/main/java/pkkwuzad1/ReverseController.java
@@ -1,4 +1,12 @@
 package pkkwuzad1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReverseController {
+    @GetMapping("/rev")
+    public String greeting() {
+        return "ver";
+    }
 }
