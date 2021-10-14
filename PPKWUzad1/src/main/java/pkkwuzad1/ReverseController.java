bd6c3d23c1ce174474fcb822ab088075a614c8d1
Mociej <maciekkocemba@gmail.com>
2021-10-14 13:01:26 +0200

    reverse input GET request

--- PPKWUzad1/src/main/java/pkkwuzad1/ReverseController.java
+++ PPKWUzad1/src/main/java/pkkwuzad1/ReverseController.java
@@ -1,12 +1,18 @@
 package pkkwuzad1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReverseController {
-    @GetMapping("/rev")
-    public String greeting() {
-        return "ver";
+    @GetMapping("/rev/{input}")
+    public String reverse(@PathVariable("input") String input) {
+        char chars[] = input.toCharArray();
+        String rev = "";
+        for (int i = chars.length - 1; i >= 0; i--) {
+            rev += chars[i];
+        }
+        return rev;
     }
 }
